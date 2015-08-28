86279ce24684443fc35ab5a6b5c8ef671949671d
chendesheng <ichendesheng@gmail.com>
2015-08-28 19:55:17 +0800

    wip

---
+++ src/webvim/change.clj
@@ -0,0 +1,29 @@
+(ns webvim.change)
+
+;A change is one edit at **single** point. 
+;For example:
+;  {:from "" :to "aa" :pos 0}
+;means insert "aa" at 0
+;:pos must always be left point of changes
+;
+;Merge changes (changes can be merged into **one** :pos):
+;  1) {:from "aaa" :to "" :pos 0} + {:from "" :to "bb" :pos 0} =>  {:from "aaa" :to "bb" :pos 0}
+;  2) {:from "" :to "aa" :pos 0} + {:from "" :to "bb" :pos 2} => {:from "" :to "aabb" :pos 0}
+;  3) {:from "abc" :to "" :pos 0} + {:from "def" :to "" :pos 0} => {:from "abcdef" :to "" :pos 0}
+;  4) {:from "" :to "abc" :pos 0} + {:from "c" :to "" :pos 2} => {:from "" :to "ab" :pos 0}
+;  ...
+;Reverse changes simply swap :from and :to:
+;  ~{:from "aaa" :to "bb" :pos 0} => {:from "bb" :to "aaa" :pos 0}
+;
+;History: [c1 c2 c3*]
+;  undo:  [c1 c2* c3] apply ~c3
+;  redo:  [c1 c2 c3*] apply c3
+
+(defn apply-changes[b changes]
+  ())
+
+(defn c+[& changes]
+  ())
+
+(defn c~[change]
+  ())
