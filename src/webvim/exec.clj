6a9b30f6122bb9c8ddecb5bde099d467c22f09bc
chendesheng <ichendesheng@gmail.com>
2016-01-17 16:03:38 +0800

    Output [Finish] for shell commands

--- src/webvim/exec.clj
+++ src/webvim/exec.clj
@@ -1,10 +1,22 @@
 (ns webvim.exec
   (:require [me.raynes.fs :as fs]
             [clj-commons-exec :as exec])
-  (:import [org.apache.commons.exec LogOutputStream]))
+  (:import [org.apache.commons.exec
+            ExecuteResultHandler
+            LogOutputStream]))
+
+(defrecord MyResultHandler [output]
+  ExecuteResultHandler
+  (onProcessComplete [_ _]
+    (output "[Finish]"))
+  (onProcessFailed [_ e]
+    (output (format "[%s]" e))))
 
 (defn exec-async[args output]
   (exec/sh args {:dir (str fs/*cwd*)
+                 :result-handler-fn (fn[result in out err opts]
+                                      (->MyResultHandler output))
                  :out (proxy [LogOutputStream] nil
                              (processLine[line]
-                               (output line)))}))
+                               (output line)))})
+  nil) ;hide result promise
