75513cf5f72ee813f44ff06ca1b939ce36da396a
chendesheng <ichendesheng@gmail.com>
2016-03-20 09:54:11 +0800

    Clojure: highlight defproject and defroutes

--- resources/public/syntax/clojure.js
+++ resources/public/syntax/clojure.js
@@ -35,7 +35,9 @@ function hlclojure(hljs) {
             'disj get union difference intersection extend extend-type extend-protocol int nth delay count concat chunk chunk-buffer ' +
             'chunk-append chunk-first chunk-rest max min dec unchecked-inc-int unchecked-inc unchecked-dec-inc unchecked-dec unchecked-negate ' +
             'unchecked-add-int unchecked-add unchecked-subtract-int unchecked-subtract chunk-next chunk-cons chunked-seq? prn vary-meta ' +
-            'lazy-seq spread list* str find-keyword keyword symbol gensym force rationalize'
+            'lazy-seq spread list* str find-keyword keyword symbol gensym force rationalize ' +
+            //not in clojure.core
+            ' defproject defroutes'
     };
 
     var SYMBOLSTART = 'a-zA-Z_\\-!.?+*=<>&#\'';
