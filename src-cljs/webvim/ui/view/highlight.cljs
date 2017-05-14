a7e55a3595007644454b3ac608227a9a6e67cc68
chendesheng <ichendesheng@gmail.com>
2017-05-14 22:41:02 +0800

    WIP

--- src-cljs/webvim/ui/view/highlight.cljs
+++ src-cljs/webvim/ui/view/highlight.cljs
@@ -3,18 +3,3 @@
     [clojure.string :as string]
     [webvim.ui.lib.dom :refer [bounding-rect $id $text-content toggle-class $hiccup]]))
 
-(defn render-highlight-inner [$lines $highlights rg]
-  ;(println "render-highlight:" rg)
-  ;(println rg)
-  (doseq [[[ax ay] [bx by]] (lines-ranges $lines rg)]
-    ;(println "range:" ax ay bx by)
-    (let [$line (-> $lines .-childNodes (aget ay))
-          [[x1 y1] [w h]] (get-rect $line ax bx)
-          [x y] (let [[linesx linesy] (rect-pos (bounding-rect $lines))]
-                  [(- x1 linesx) (- y1 linesy)])]
-      (.appendChild $highlights
-                    ($hiccup [:span.line-selected
-                              {:style (str "left:" x "px;" "top:" y "px;"
-                                           "width:" w "px;" "height:" (line-height) "px;"
-                                           "padding-right:1ch;")}])))))
-
